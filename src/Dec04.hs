b20be833ee1ba70334b598c140e26dff0fbd0aa2
theindigamer <theindigamer15@gmail.com>
2018-12-04 21:15:07 -0500

    Clean up Dec 04 implementation.

--- src/Dec04.hs
+++ src/Dec04.hs
@@ -3,35 +3,30 @@ module Dec04 where
 
 import Common
 import Data.Semigroup
-import Data.List (sortBy, sort, foldl')
 import Data.Function (on)
+import Data.Hashable
 
 import Debug.Trace
 
 import qualified Data.HashMap.Strict as HM
+import qualified Data.List as List
 import qualified Text.Megaparsec.Char as C
 
-data GS = WakeUp | Sleep | Begin Int
+data GuardState = WakeUp | Sleep | Begin GuardId
   deriving (Eq, Ord, Show)
 
 data Entry =
-  Entry { month :: Int, day :: Int, hour :: Int, mins :: Int, info :: GS }
+  Entry { month :: Int, day :: Int, hour :: Int, mins :: Int, info :: GuardState }
   deriving (Eq, Ord, Show)
 
-gsP :: Parser String GS
-gsP =
-  C.string "falls asleep" *> pure Sleep
-  <|> C.string "wakes up" *> pure WakeUp
-  <|> (C.string "Guard #" *> (Begin <$> signedIntP) <* takeWhile1P Nothing (const True))
-
-entryP :: Parser String Entry
-entryP =
-  Entry
-  <$> (C.string "[1518-" *> signedIntP)
-  <*> (C.char '-' *> signedIntP)
-  <*> (C.space1 *> signedIntP)
-  <*> (C.char ':' *> signedIntP)
-  <*> (C.string "] " *> gsP)
+guardStateP :: Parser String GuardState
+guardStateP =
+      Sleep  <$ C.string "falls asleep"
+  <|> WakeUp <$ C.string "wakes up"
+  <|> (  C.string "Guard #"
+      *> (Begin <$> signedIntP)
+      <* takeWhile1P Nothing (const True)
+      )
 
 testcase =
   "[1518-11-01 00:00] Guard #10 begins shift\n\
@@ -46,58 +41,58 @@ testcase =
   \[1518-11-03 00:05] falls asleep\n\
   \[1518-11-03 00:25] wakes up"
 
+entryP :: Parser String Entry
+entryP =
+  Entry
+  <$> (C.string "[1518-" *> signedIntP)
+  <*> (C.char '-' *> signedIntP)
+  <*> (C.space1 *> signedIntP)
+  <*> (C.char ':' *> signedIntP)
+  <*> (C.string "] " *> guardStateP)
+
+oneOrPlusOne :: (Eq k, Hashable k) => k -> HashMap k Int -> HashMap k Int
+oneOrPlusOne = HM.alter (Just . maybe 1 (+ 1))
+
+minsAsleep :: Entry -> Entry -> [Int]
+minsAsleep sleep wake = [mins sleep .. mins wake - 1]
+
+type GuardId = Int
+
+go
+  :: (GuardId -> HashMap k v -> [Int] -> HashMap k v)
+  -> (HashMap k v, Maybe Entry, Maybe Int)
+  -> Entry
+  -> (HashMap k v, Maybe Entry, Maybe Int)
+go f (h, prev, cur) e@Entry{info} = case info of
+  Begin i -> (h, Nothing, Just i)
+  Sleep -> (h, Just e, cur)
+  WakeUp -> case (prev, cur) of
+    (Just p, Just i) -> (f i h (minsAsleep p e), Nothing, Just i)
+    _ -> error "Unreachable"
+
 dec04P1 :: IO ()
 dec04P1 =
-  -- linewise (pure testcase) $
   linewise (readFile "data/dec04.txt") $
       map (justParse entryP)
-      .> sort
-      .> foldl' go (HM.empty, Nothing, Nothing)
+      .> List.sort
+      .> List.foldl' (go f) (HM.empty, Nothing, Nothing)
       .> (\(x, y, z) -> x)
       .> HM.toList
       .> map (second (\(HM.toList -> xs) -> (sum $ map snd xs, xs)))
-      .> sortBy (flip compare `on` snd)
-      .> head
-      .> second (head . sortBy (flip compare `on` snd) . snd)
+      .> List.maximumBy (compare `on` snd)
+      .> second (List.maximumBy (compare `on` snd) . snd)
   where
-    minsBetween :: Entry -> Entry -> [Int]
-    minsBetween sleep wake = [mins sleep .. mins wake - 1]
-    f i sleepinfo wakeinfo hm =
-      foldl' (\h m -> HM.alter
-             (\case {Nothing -> Just (HM.fromList [(m, 1)]);
-                     Just g -> Just (HM.alter (\case {Nothing -> Just 1;
-                                                      Just i -> Just (i + 1);})
-                                      m g)
-                    }
-             ) i h) hm (minsBetween sleepinfo wakeinfo)
-    go (h, prev, cur) e@Entry{info} = case info of
-      Begin i -> (h, Nothing, Just i)
-      Sleep -> (h, Just e, cur)
-      WakeUp -> case (prev, cur) of
-        (Just p, Just i) -> (f i p e h, Nothing, Just i)
-        _ -> error "Unreachable"
+    f i = foldl' $ \h m -> HM.alter
+        (Just . maybe (HM.fromList [(m, 1)]) (oneOrPlusOne m)) i h
 
 dec04P2 :: IO ()
 dec04P2 =
-  -- linewise (pure testcase) $
   linewise (readFile "data/dec04.txt") $
       map (justParse entryP)
-      .> sort
-      .> foldl' go (HM.empty, Nothing, Nothing)
+      .> List.sort
+      .> List.foldl' (go f) (HM.empty, Nothing, Nothing)
       .> (\(x, y, z) -> x)
       .> HM.toList
-      .> sortBy (flip compare `on` snd)
-      .> head
+      .> List.maximumBy (compare `on` snd)
   where
-    minsBetween :: Entry -> Entry -> [Int]
-    minsBetween sleep wake = [mins sleep .. mins wake - 1]
-    f i sleepinfo wakeinfo hm =
-      foldl' (\h m -> HM.alter
-             (\case {Nothing -> Just 1; Just k -> Just (k + 1)})
-             (i, m) h) hm (minsBetween sleepinfo wakeinfo)
-    go (h, prev, cur) e@Entry{info} = case info of
-      Begin i -> (h, Nothing, Just i)
-      Sleep -> (h, Just e, cur)
-      WakeUp -> case (prev, cur) of
-        (Just p, Just i) -> (f i p e h, Nothing, Just i)
-        _ -> error "Unreachable"
+    f i = foldl' $ \h m -> oneOrPlusOne (i, m) h
