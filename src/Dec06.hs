7422a27218e8d02765e295369418080236c07bbb
theindigamer <theindigamer15@gmail.com>
2018-12-06 01:42:44 -0500

    Clean up Dec 6 solution.

--- src/Dec06.hs
+++ src/Dec06.hs
@@ -1,41 +1,36 @@
 module Dec06 where
 
 import Common
-import Data.Char
-import Data.Maybe
-import Data.Function
 import Data.List
-import Data.Bifunctor
+import Control.Arrow
 
-data Coords = C { x :: Int, y :: Int }
+data Coord = C { x :: Int, y :: Int }
   deriving (Eq, Ord, Show)
 
-isOnBoundary :: Coords -> [Coords] -> Bool
+isOnBoundary :: Coord -> [Coord] -> Bool
 isOnBoundary C{x, y} cs =
-  null [() | C x1 y1 <- cs, x1 < x && y1 < y]
+     null [() | C x1 y1 <- cs, x1 < x && y1 < y]
   || null [() | C x1 y1 <- cs, x1 > x && y1 < y]
   || null [() | C x1 y1 <- cs, x1 < x && y1 > y]
   || null [() | C x1 y1 <- cs, x1 > x && y1 > y]
 
-segregate :: [Coords] -> ([Coords], [Coords])
+segregate :: [Coord] -> ([Coord], [Coord])
 segregate cs =
-  (bimap (map snd) (map snd)) $ partition fst $ zip (map (`isOnBoundary` cs) cs) cs
+   (map snd *** map snd) $ partition fst $ zip (map (`isOnBoundary` cs) cs) cs
 
-getPoint :: String -> Coords
+getPoint :: String -> Coord
 getPoint s =
-  let x = read $ (takeWhile (/= ',')) s
+  let x = read $ takeWhile (/= ',') s
       y = read $ (dropWhile (== ' ') . dropWhile (/= ' ')) s
   in C{x, y}
 
 dist (C x1 y1) (C x2 y2) = abs (x1 - x2) + abs (y1 - y2)
 
--- f :: Coords -> [Coords] -> Maybe (Coords, Int)
-f b c cs = case sortOn snd $ map (\c' -> (c', dist c c')) cs of
-  (c1, d1):(c2, d2):_ | (b || d1 < d2) -> Just (c1, d1)
-  _ -> Nothing
-
-cmpx = comparing x
-cmpy = comparing y
+getClosest :: Bool -> Coord -> [Coord] -> Maybe (Coord, Int)
+getClosest uniq c cs =
+  case sortOn snd $ map (\c' -> (c', dist c c')) cs of
+    (c1, d1):(c2, d2):_ | not uniq || d1 < d2 -> Just (c1, d1)
+    _ -> Nothing
 
 testcase =
   "1, 1\n\
@@ -46,55 +41,56 @@ testcase =
   \8, 9"
 
 dec06P1 :: IO ()
--- dec06P1 = linewise (pure testcase) $
 dec06P1 = linewise (readFile "data/dec06.txt") $
   map getPoint .> segregate .>
   (\(bounds, ins) ->
-     let xmin = x $ minimumBy cmpx bounds
-         ymin = x $ minimumBy cmpx bounds
-         xmax = y $ maximumBy cmpy bounds
-         ymax = y $ maximumBy cmpy bounds
-     -- in ins
-     in (length &&& id)
-       $ maximumBy (comparing length)
-       $ group $ sort
-        $ [c | x <- [xmin .. xmax], y <- [ymin .. ymax]
-            , let (r, c) = case (f True C{x, y} bounds, f False C{x,y} ins) of
-                    (Just bdist, Just indist) -> (snd indist < snd bdist, fst indist)
-                    (Just bdist, Nothing) -> (False, C 0 0)
-                    _ -> (False, C 0 0)
-            , r
-          ]
+     let xs = sort $ map x bounds
+         xmin = head xs
+         xmax = last xs
+         ys = sort $ map y bounds
+         ymin = head ys
+         ymax = last ys
+     in [ c | x <- [xmin .. xmax], y <- [ymin .. ymax]
+        , let (closeToInner, c) =
+                case (getClosest False C{x, y} bounds, getClosest True C{x,y} ins) of
+                  (Just bdist, Just indist) -> (snd indist < snd bdist, fst indist)
+                  (Just bdist, Nothing) -> (False, C 0 0)
+                  _ -> error "Unreachable" -- Ah, this is not nice :(
+        , closeToInner
+        ]
+        |> sort
+        |> group
+        |> maximumBy (comparing length)
+        |> length &&& id
   )
 
-alternate (x:xs) (y:ys) = x:y:alternate xs ys
-
-deltas d c@C{x, y} =
+deltas :: Coord -> Int -> [Coord]
+deltas c@C{x, y} d =
   if d == 0 then [c]
-  else concatMap (\dx -> let dy = d - dx in
-                [C (x + dx) (y + dy)
-                , C (x - dx) (y - dy)
-                , C (x + dx) (y - dy)
-                , C (x - dx) (y + dy)
-                ]) [0 .. d]
+  else concatMap
+    (\dx -> let dy = d - dx in
+        [ C (x + dx) (y + dy)
+        , C (x - dx) (y - dy)
+        , C (x + dx) (y - dy)
+        , C (x - dx) (y + dy)
+        ]
+    )
+    [0 .. d]
 
+dec06P2 :: IO ()
 dec06P2 = linewise (readFile "data/dec06.txt") $
   map getPoint .>
   (\cs ->
      let np = length cs
          xs = sort $ map x cs
-         xmin = head xs
-         xmax = last xs
          ys = sort $ map y cs
-         ymin = head ys
-         ymax = last ys
-         xmid = (xmin + xmax) `div` 2
-         ymid = (ymin + ymax) `div` 2
+         xmid = (head xs + last xs) `div` 2
+         ymid = (head ys + last ys) `div` 2
          sumDist c = sum $ map (dist c) cs
          d = 10000
-     in
-     sum $ map (length . nub)
-     $ takeWhile (not . null)
-     $ map (\d' -> filter ((< d) . sumDist) $ deltas d' (C xmid ymid))
-     $ [0 ..]
+     in [0 ..]
+        |> map (deltas (C xmid ymid) .> filter (sumDist .> (< d)))
+        |> takeWhile (not . null)
+        |> map (nub .> length)
+        |> sum
   )
